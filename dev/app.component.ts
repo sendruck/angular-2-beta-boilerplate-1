7579a37d70a89f4c2596a0fcccac61cd57c7eef5
sendruck <olexandr.yakov@gmail.com>
2016-11-05 18:55:58 +0200

    New something

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,29 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
-})
+          <h3 
+          (click)="onSelect()"
+          [class.clicked]="showDetail === true"
+          >{{contact.firstName}}</h3>
+          <input [(ngModel)]="contact.firstName" type="text"/>
+          <div *ngIf="showDetail === true">
+            Phone Number: {{contact.phone}} <br>
+            E-Mail: {{contact.email}}
+          </div>
+`,
+      styleUrls: ["../src/css/app.css"]
+}
+
+
+)
 export class AppComponent {
+    public contact = {firstName: "Sasha", lastName: "Yakovenko",
+      phone: "234 234", email: "ohyrec@gmail.com"};
+
+    public showDetail = false;
+
+    onSelect() {
+      this.showDetail = true;
+    }
 
 }
