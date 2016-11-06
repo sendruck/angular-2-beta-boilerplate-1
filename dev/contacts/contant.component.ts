aafb2c1785e72c85e5e538620f74d589d5adc62c
Alexandr Yakovenko <olexandr.yakov@gmail.com>
2016-11-06 22:28:56 +0200

    Multiple Components

--- dev/contacts/contant.component.ts
+++ dev/contacts/contant.component.ts
@@ -3,13 +3,37 @@ import {Component} from "angular2/core";
 @Component({
   selector: "contact",
   template: `
-      <input [(ngModel)]="contact.firstName" type="text"/>
           <div>
-            Phone Number: {{contact.phone}} <br>
-            E-Mail: {{contact.email}}
+            <label for="first-name">First Name:</label>            
+            <input [(ngModel)]="contact.firstName" type="text"/>
+          </div>
+          <div>
+             <label for="last-name">Last Name:</label>
+            <input [(ngModel)]="contact.lastName" type="text"/>
+          </div>
+          <div>
+            <label for="phone">Phone Number:</label>            
+            <input [(ngModel)]="contact.phone" type="text"/>
+          </div>
+          <div>
+            <label for="email">E-Mail:</label>            
+            <input [(ngModel)]="contact.email" type="text"/>
+          </div>
+          <div>
+           
           </div>
 `,
-      inputs: ["contact"]
+      inputs: ["contact"],
+      styles: [`
+          label {
+               display: inline-block;
+              width: 140px;
+          }
+          
+          input {
+              width: 250px;
+          }
+`]
 })
 export class ContactComponent {
     public contact = {};
